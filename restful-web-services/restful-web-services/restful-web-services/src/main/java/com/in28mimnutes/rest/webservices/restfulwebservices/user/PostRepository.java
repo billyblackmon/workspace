dc9cfe8ed15ca0c7c716965166091c48ca7d4038
billyblackmon <33910811+billyblackmon@users.noreply.github.com>
2017-12-11 15:47:14 -0600

    Add files via upload
    Class completed. On to 'Master Microservices with Spring Boot and Spring Cloud'. See MasterWebServicesCert..jpg for certificate of completion.

--- restful-web-services/restful-web-services/restful-web-services/src/main/java/com/in28mimnutes/rest/webservices/restfulwebservices/user/PostRepository.java
+++ restful-web-services/restful-web-services/restful-web-services/src/main/java/com/in28mimnutes/rest/webservices/restfulwebservices/user/PostRepository.java
@@ -3,8 +3,6 @@ package com.in28mimnutes.rest.webservices.restfulwebservices.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// import com.in28minutes.rest.services.springboot.bean.Post;
-// import com.in28minutes.rest.services.springboot.bean.User;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
